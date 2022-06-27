dad18fdf946e22eccc1a46391da6ef475c736802
stefzouille <contact.lokcy@gmail.com>
2022-06-27 11:32:21 +0200

    add typo com

--- app/routes/user.js
+++ app/routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 
-// creér 2 routes POST pour les users 
+// creér routes POST pour les users 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.delete('/:id', userCtrl.delete);
