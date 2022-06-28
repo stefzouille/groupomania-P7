a56d1f7cac476daf6769e61e7aeec41edd4339c1
stefzouille <contact.lokcy@gmail.com>
2022-06-28 16:35:58 +0200

    struct reactapp add banner test

--- frontend_groupomania/src/index.js
+++ frontend_groupomania/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import '../src/styles/index.css';
 import App from './components/App';
+import Banner from './components/Banner';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Banner />
     <App />
   </React.StrictMode>
 );
