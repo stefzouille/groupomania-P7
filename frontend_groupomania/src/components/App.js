a4bd0c83b467b4dc394ad72d0b5e3a8164d25aae
stefzouille <contact.lokcy@gmail.com>
2022-07-06 17:50:06 +0200

    add router pb

--- frontend_groupomania/src/components/App.js
+++ frontend_groupomania/src/components/App.js
@@ -1,21 +1,38 @@
 import logo from './logo.svg';
 import '../App.css';
-import FormSignup from './Form_signup';
+// import FormSignup from './FormSignup';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      {/* bouton renvoie vers la page de signup */}
+    // <BrowserRouter>
+    //   <Link to="/signup" /> FormSignup </Link>
+    // </BrowserRouter >
+    // route pour la page d'inscription
+    <BrowserRouter>
+      <div>
+
+        <Link to="/">Home</Link>
+      </div>
 
 
 
-      <FormSignup />
+      <div className="App">
+        {/* bouton renvoie vers la page de signup */}
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+
+
+        {/* <FormSignup /> */}
+
+
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    </BrowserRouter>
+  )
 }
+
 
 export default App;
