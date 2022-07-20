a7ba7cf0cc7f60c8498120ca12f61b101c967750
stefzouille <contact.lokcy@gmail.com>
2022-07-20 14:37:52 +0200

    useeffect not work

--- frontend_groupomania/src/components/Home.js
+++ frontend_groupomania/src/components/Home.js
@@ -1,14 +1,78 @@
 // page acceuil
 import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 // import logo from './logo.svg';
 {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header> */}
 
+// recuperer le post de la base de données
+// function SeePosts() {
+//   const { posts, setPosts } = useState([]);
+//   useState(() => {
+//     fetch('http://localhost:5000/post', {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': 'Bearer ' + localStorage.getItem('token')
+//       }
+//     })
+//       .then(response => response.json())
+//       .then(data => {
+//         if (data.token) {
+//           localStorage.setItem('token', data.token);
+//           // afficher les post de la base de données sur le dom
+//           window.location.href = '/Home';
+//           console.log(data);
+//         }
+//         setPosts(data);
+//       })
+//       .catch(error => console.log(error));
+//   }
+//     , []);
+// }
+// const [isDataLoading, setDataLoading] = useState(false)
+// const [posts, setPosts] = useState([])
+
+// useEffect(() => {
+//   setDataLoading(true)
+//   fetch(`http://localhost:5000/post`)
+//     .then((response) => response.json())
+//     .then(({ }) => {
+
+//       setDataLoading(false)
+//     })
+// }, [])
+
 class Home extends Component {
 
   render() {
     if (localStorage.getItem('token')) {
+
+      // function SeePosts() {
+      //   const { posts, setPosts } = useState([]);
+      //   useState(() => {
+      //     fetch('http://localhost:5000/post', {
+      //       method: 'GET',
+      //       headers: {
+      //         'Content-Type': 'application/json',
+      //         'Authorization': 'Bearer ' + localStorage.getItem('token')
+      //       }
+      //     })
+      //       .then(response => response.json())
+      //       .then(data => {
+      //         if (data.token) {
+      //           localStorage.setItem('token', data.token);
+      //           // afficher les post de la base de données sur le dom
+      //           window.location.href = '/Home';
+      //           console.log(data);
+      //         }
+      //         setPosts(data);
+      //       })
+      //       .catch(error => console.log(error));
+      //   }
+      //     , []);
+
 
 
       return (
@@ -20,38 +84,13 @@ class Home extends Component {
           }}> Créer un post
           </button>
           <hr />
-
-          {/* <input type="text" onChange={(e) => { getPosts(e.target.value) }} /> */}
           <p>posts recents</p>
+          {/* afficher les posts de la base de données */}
+
 
         </div>
-      );
-      // recuperer le post de la base de données
-      function getPosts() {
-        fetch('http://localhost:5000/post', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + localStorage.getItem('token')
-          }
-        })
-          .then(response => response.json())
-          .then(data => {
-            if (data.token) {
-              localStorage.setItem('token', data.token);
-              // afficher les post de la base de données sur le dom
-              window.location.href = '/Home';
-              console.log(data);
-            }
+      )
 
-          }
-          )
-          .catch(error => {
-            console.log(error);
-          }
-          )
-        getPosts();
-      }
 
     } else {
       window.location.href = '/Login';
