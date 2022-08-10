b5332fb27a6226a2a6f00b6f241735f5d9c47c0f
stefzouille <contact.lokcy@gmail.com>
2022-08-10 17:19:58 +0200

    verif modify & delete not work

--- frontend_groupomania/src/components/Post.js
+++ frontend_groupomania/src/components/Post.js
@@ -2,13 +2,9 @@ import React from "react";
 // import '../styles/Card.css';
 import ModifyPost from './ModifyPost';
 import '../styles/Post.css';
-import jwt_decode from 'jwt-decode';
-
-
 
 
 function Post(props) {
-
 
   function deletePost(id) {
     fetch('http://localhost:5000/post/' + id, {
@@ -29,196 +25,31 @@ function Post(props) {
   }
 
 
-  // afficher l user name qui a créer le post
-  function getUserName(id) {
-    fetch('http://localhost:5000/post/' + id, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('token')
-      }
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        return data.userName;
-      }).catch(error => {
-        console.log(error);
-      }
-      )
-  }
 
-  // afficher le contenu du post
-  function getContent(id) {
-    fetch('http://localhost:5000/post/' + id, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('token')
-      }
-    })
-
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        return data.description;
-      }).catch(error => {
-        console.log(error);
-      }
-      )
-  }
-
-  // afficher le contenu du post
-  function getTitle(id) {
-    fetch('http://localhost:5000/post/' + id, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('token')
-      }
-    })
-
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        return data.title;
-      }).catch(error => {
-        console.log(error);
-      }
-      )
-  }
-
-  function getDescription(id) {
-    fetch('http://localhost:5000/post/' + id, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('token')
-      }
-    })
-
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        return data.description;
-      }).catch(error => {
-        console.log(error);
-      }
-      )
-  }
-  // afficher le contenu du post
-  function getDate(id) {
-    fetch('http://localhost:5000/post/' + id, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('token')
-      }
-    })
-
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        return data.date;
-      }).catch(error => {
-        console.log(error);
-      }
-      )
-  }
-  // afficher le contenu du post
-  function getId(id) {
-    fetch('http://localhost:5000/post/' + id, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('token')
-      }
-    })
-
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        return data.id;
-      }).catch(error => {
-        console.log(error);
-      }
-      )
-  }
-
-  // // afficher le contenu du post
-  // function getUserId(id) {
-  //   fetch('http://localhost:5000/post/' + id, {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Authorization': 'Bearer ' + localStorage.getItem('token')
-  //     }
-  //   })
-
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       console.log(data);
-  //       return data.userId;
-  //     }).catch(error => {
-  //       console.log(error);
-  //     }
-  //     )
-  // }
-
-  // afficher le contenu du post
-  function getImage(id) {
-    fetch('http://localhost:5000/post/' + id, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('token')
-      }
-    })
-
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        return data.image;
-      }).catch(error => {
-        console.log(error);
-      }
-      )
-  }
-
-
-
+  console.log(props);
   return (
     <div className="card-cont" >
       <div className="card-body">
-        {/* <h5 className="card-title">{props.title}</h5> */}
-        {/* <p className="card-text">{props.content}</p> */}
-        {/* <h2> {getUserName()}</h2> */}
         <h3 className="card-title">{props.post.title}</h3>
+
         <p className="card-text">{props.post.description}</p>
-
-        <p className="card-text">{props.post.date}</p>
-        <p className="card-text">{props.post.userName}</p>
-
-
-        {/* <p className="card-text">{props.post.description}</p> */}
-
         {/* affiche l'image du post */}
-        <img src={props.post.image} alt="post" className="card-img" />
-
-        {/* <img className="card-img-top" src={props.post.image} alt="Card cap" /> */}
+        <img className="card-img-top" src={props.post.image} alt="Card cap" />
         <hr />
 
         {/* affiche le boutton modifier */}
         <button className="boutoModif" onClick={() => {
           // seulement l user qui a ecrit le post peut le modifier
-          // if (getUserId(props.post.id) === getUserId(localStorage.getItem('userId'))) {
+          if (props.userCreated === localStorage.getItem('userId')) {
+            ModifyPost(props.post.id);
+          } else {
+            alert('vous ne pouvez pas modifier ce post');
+          }
 
-
-          //   console.log('ok');
-          //   return <ModifyPost id={props.post.id} />
-
+          // if (props.post.userCreated === localStorage.getItem('userId')) {
+          //   ModifyPost(props.post.id)
           // } else {
-          //   alert('Vous n\'avez pas le droit de modifier ce post');
+          //   alert('Vous n\'avez pas le droit de supprimer ce post');
           // }
         }
         }>Modifier</button>
@@ -226,12 +57,18 @@ function Post(props) {
         {/* affiche button delete  */}
         <button className="boutoSupp" onClick={() => {
           // seulement l user qui a ecrit le post peut le supprimer
-
-          if (props.post.user_id === localStorage.getItem('user_id')) {
+          if (props.post.userCreated === localStorage.getItem('userId')) {
             deletePost(props.post.id)
           } else {
             alert('Vous n\'avez pas le droit de supprimer ce post');
           }
+
+          console.log(props.post.userCreated);
+          // if (props.post.id === localStorage.getItem('userId')) {
+          //   deletePost(props.post.id)
+          // } else {
+          //   alert('Vous n\'avez pas le droit de supprimer ce post');
+          // }
         }
         }> Supprimer
         </button>
@@ -240,8 +77,6 @@ function Post(props) {
     </div >
   )
 }
-
-
 
 
 export default Post;
